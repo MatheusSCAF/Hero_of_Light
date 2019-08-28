177b6751c6a6aa2b214bd864c45114d5c617e24a
Matheus Sales <matheus_salles@nave.org.br>
2019-08-28 20:40:22 -0300

    Sistema Pause e Organização Pastas

---
+++ Invasion of the clock/Assets/Script/Controller/PauseController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseController : MonoBehaviour
+{  
+    
+    //Declaração Variavel
+    //Declaração Métodos
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    
+    
+    
+    void Update()
+    {
+        //Void de checagem
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+            
+
+        }
+       
+
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Go to Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+    }
+
+}
