acc41312a37a4ef91b10458fb2d4b1c6c923b3ee
Matheus Sales <afre.luz@nave.org.br>
2019-10-03 12:05:23 -0300

    Areas secretas implementadas

---
+++ Invasion of the clock/Assets/areasInvisiveisController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areasInvisiveisController : MonoBehaviour
+{
+    SpriteRenderer render;
+
+    void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(FadeOut());
+        }   
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(FadeIn());
+        }
+    }
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = render.material.color;
+            c.a = f;
+            render.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.05f; f <= 1f; f += 0.05f)
+        {
+            Color c = render.material.color;
+            c.a = f;
+            render.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
