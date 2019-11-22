bc862e34126264684001e01657d8c9299645afa9
Rick-Melo <Afre.luz@gmail.com>
2019-11-22 10:05:08 -0300

    atualizacao

---
+++ Invasion of the clock/Assets/VoltarCena.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class VoltarCena : MonoBehaviour
+{
+    public Button voltar;
+    public string nomeCena;
+    public 
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        voltar.onClick.AddListener(() => Voltar());
+    }
+
+    public void Voltar()
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+}
