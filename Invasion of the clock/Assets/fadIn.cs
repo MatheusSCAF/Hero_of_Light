ecf8b516b2216c803eed356f44fc63a123a1b216
Rick-Melo <Afre.luz@gmail.com>
2019-11-28 07:55:26 -0300

    Finalização game

---
+++ Invasion of the clock/Assets/fadIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class fadIn : MonoBehaviour
+{
+    public SpriteRenderer render;
+    private void Start()
+    {
+            StartCoroutine(FadeOut());
+    }
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = render.material.color;
+            c.a = f;
+            render.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+        SceneManager.LoadScene("Menu");
+    }
+}
